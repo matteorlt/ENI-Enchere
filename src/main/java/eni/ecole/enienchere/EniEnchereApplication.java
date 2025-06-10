17e57ce1087265a44b7f71b0b8296aacaa6d0093
Matteo RANNOU LE TEXIER <matteo.rannouletexier2025@campus-eni.fr>
2025-06-10 11:34:51 +0200

    Initial commit

---
+++ src/main/java/eni/ecole/enienchere/EniEnchereApplication.java
@@ -0,0 +1,13 @@
+package eni.ecole.enienchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniEnchereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EniEnchereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eni/ecole/enienchere/EniEnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package eni.ecole.enienchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniEnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
